6df24b114db232ec557490517530ebdac3299c78
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-18 17:29:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+public class Main implements ActionListener{
+  
+  JFrame frame;
+  JTextField textField;
+  JButton[] numberButtons = new JButton[10];
+  JButton[] functionButtons = new JButton[8];
+  JButton addButton, subButton, mulButton, divButton;
+  JButton decButton, equButton, delButton, clrButton;
+  JPanel panel;
+
+  Font myFont = new Font("Ink Free", Font.BOLD, 30);
+  double num1 = 0, num2 = 0, result=0;
+  char operator;
+  
+  Calculator(){
+    
+  }
+  public static void main(String[] args) {
+    
+    Calculator calc = new Calculator();
+    
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e){
+    
+  }
+}
